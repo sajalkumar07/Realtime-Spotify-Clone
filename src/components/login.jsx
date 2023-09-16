1553d38a91b46ac2550c422dda7ed72e5c391a4d
Sajal <sajalkumarr030@gmail.com>
2023-09-16 12:36:02 +0530

    update changes

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,9 +1,30 @@
 import React from 'react'
 import styled from 'styled-components'
-
+import logo from './images/logo.png'
 export const Login = () => {
+
+  const handleClick = () =>  {
+    const clientId = "3548ed7ae5f346a1b8c4245c01196eef";
+    const redirectUrl = "http://localhost:3000/";
+    const apiUrl =  "https://accounts.spotify.com/authorize";
+    const scope  = [
+      'user-read-email',
+      'user-read-private',
+      'user-read-playback-state',
+      'user-read-currently-playing',
+      'user-read-recently-played',
+      'user-read-playback-position',
+      'user-top-read'];
+
+      window.location.href = `${apiUrl}?client_id=${clientId}&redirect_uri=${redirectUrl}&scope=${scope.join(" ")}
+      &response_type=token&show_daialog=true`;
+    };
+
   return (
-    <Container>yoo</Container>
+    <Container className='container'>
+      <img className='logo' src={logo} alt=""/>
+      <button onClick={handleClick} className='login-button'>Connect Spotify</button>
+    </Container>
   )
 }
 export default Login
