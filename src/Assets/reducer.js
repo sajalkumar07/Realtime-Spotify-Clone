48296cc507efdc22f846e187b1c67fc0662c8040
Sajal <sajalkumarr030@gmail.com>
2023-09-30 16:21:04 +0530

    update changes

--- src/Assets/reducer.js
+++ src/Assets/reducer.js
@@ -4,28 +4,55 @@ export const initialState  = {
     token: null,
     playlists: [],
     userInfo : null,
-    selectedPlaylistId: '37i9dQZF1EQncLwOalG3K7?si=e35b68cbd94f4550',
+    playlists: [],
+    currentPlaying: null,
+    platerState: false,
+    selectedPlaylistId: '23K3adIToeG65Lb2NgBJ4L',
+    selectedPlaylist: null,
 }
 const reducer = (state, action) =>{
     switch (action.type){
         case reducerCases.SET_TOKEN:{
             return{
-                ...state, token: action.token,
+                ...state, 
+            token: action.token,
             }
         }
         case reducerCases.SET_PLAYLISTS:{
             return{
                 ...state,
-                playlists:  action.playlists,
+            playlists:  action.playlists,
             }
         }
-
         case reducerCases.SET_USER : {
             return{
                 ...state,
             userInfo: action.userInfo
             }
         }
+        case reducerCases.SET_PLAYLIST:{
+            return{
+                ...state,
+            selectedPlaylist: action.selectedPlaylist
+            }
+        }
+        case reducerCases.SET_PLAYING:{
+            return{
+                ...state,
+            currentPlaying: action.currentPlaying
+            }
+        }
+        case reducerCases.SET_PLAYER_STATE:{
+            return{
+                ...state,
+            platerState: action.platerState
+            }
+        }
+        case reducerCases.SET_PLAYLIST_ID:
+            return {
+                ...state,
+            selectedPlaylistId: action.selectedPlaylistId,
+        };
         default:
             return state
     }
--- src/components/body.jsx
+++ src/components/body.jsx
@@ -1,32 +1,124 @@
-import React, {useEffect} from 'react'
-import '../styles/body.css'
-import styled from 'styled-components'
-import {AiFillClockCirlce} from "react-icons/ai"
-import { useStateProvider } from '../Assets/SatateProvider'
 import axios from 'axios'
+import React, {useEffect} from 'react'
+import styled from 'styled-components'
+import '../styles/body.css'
+import { useStateProvider } from '../Assets/SatateProvider'
+import {AiFillClockCircle} from "react-icons/ai"
+import { reducerCases } from '../Assets/constents'
+const Body = ({headerBackground}) => {
 
-const Body = () => {
-
-  const[{token, selectedPlaylistId}, dispatch]=useStateProvider();
+  const[{token, selectedPlaylist, selectedPlaylistId}, dispatch] = useStateProvider();
   useEffect(() =>{
     const getInitialPlaylist = async () => {
-      const response  = await axios.get(`https://api.spotify.com/v1/playlists/${selectedPlaylistId}`, {
+      const response  = await axios.get(
+        `https://api.spotify.com/v1/playlists/${selectedPlaylistId}`, 
+      {
         headers:{
           Authorization: "Bearer  "+ token,
           "Content-Type": "application/json",
         },
-    })
-      
-      console.log(response)
-    }
+      }
+    );
+      const selectedPlaylist = {
+        id : response.data.id,
+        name: response.data.name,
+        description: response.data.description.startsWith("<a")
+        ?""
+        : response.data.description,
+        image: response.data.images[0].url,
+        tracks: response.data.tracks.items.map(({track})=>({
+          id: track.id,
+          name: track.name,
+          artists: track.artists.map((artist)=>artist.name),
+          image: track.album.images[2].url,
+          duration: track.duration_ms,
+          album: track.album.name,
+          context_uri: track.album.uri,
+          track_number: track.track_number,
+        })),
+    };
+    // console.log(selectedPlaylist)
+    // console.log(response.data)
+    dispatch({type: reducerCases.SET_PLAYLIST, selectedPlaylist});
+  };
     getInitialPlaylist()
-  },[token, dispatch]);
+  },[token, dispatch, selectedPlaylistId]);
   return (
-    <Container>
-      
+    <Container headerBackground={headerBackground}>
+      {
+        selectedPlaylist && (
+          <>
+            <div className="playlist">
+              <div className="image">
+                <img src={selectedPlaylist.image} alt="" />
+              </div>
+              <div className="details">
+                <span className="type">PLAYLIST</span>
+                <h1 className="title">{selectedPlaylist.name}</h1>
+                <p className="description">{selectedPlaylist.description}</p>
+              </div>
+            </div>
+
+            <div className="list">
+              <div className="header-row">
+                <div className="col">
+                  <span>#</span>
+                </div>
+                <div className="col">
+                  <span>TITLE</span>
+                </div>
+                <div className="col">
+                  <span>ALBUM</span>
+                </div>
+                <div className="col">
+                  <span><AiFillClockCircle>
+                    </AiFillClockCircle>
+                  </span>
+                </div>
+              </div>
+              <div className="tracks">
+                {
+                  selectedPlaylist.tracks.map(({
+                    id,
+                    name,
+                    artists,
+                    image,
+                    duration,
+                    album,
+                    context_uri,
+                    track_number,
+                  },index)=>{
+                    return(
+                      <div className="row">
+                        <div className="col">
+                          <span>{index+1}</span>
+                        </div>
+                        <div className="col-detail">
+                          <div className="image">
+                            <img src={image} alt="" />
+                            <div className="info">
+                              <span className='name'>{name}</span>
+                              <span>{artists}</span>
+                            </div>
+                          </div>
+                          <div className="col">
+                            <span>{album}</span>
+                          </div>
+                          <div className="col">
+                            <span>{duration}</span>
+                          </div>
+                        </div>
+                      </div>
+                    )
+                  })
+                }
+              </div>
+            </div>
+          </>
+        )
+      }
     </Container>
   )
 }
-
 export default Body
 const Container = styled.div``
